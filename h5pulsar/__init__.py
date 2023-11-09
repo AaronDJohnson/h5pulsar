6971d6e8ce7340444392d899cae30ae5d97efd8f
Rutger van Haasteren <vhaasteren@gmail.com>
2023-11-09 13:30:15 +0100

    Modified the setup system

--- h5pulsar/__init__.py
+++ h5pulsar/__init__.py
@@ -1,1 +1,7 @@
 from .h5pulsar import PintPulsar, Tempo2Pulsar, Pulsar, FilePulsar, BasePulsar
+
+try:
+    from ._version import __version__
+except ImportError:
+    # _version.py will be missing during package setup when setuptools_scm has not yet written it
+    __version__ = "unknown"
--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="h5pulsar",
-    version="1.1.0",
+    use_scm_version={
+        "write_to": "h5pulsar/_version.py",
+        "write_to_template": "__version__ = '{version}'",
+    },
     author="Rutger van Haasteren",
     author_email="rutger@vhaasteren.com",
-    description="HDF5 read/write capabilities for Enterprise Pulsar objects",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/vhaasteren/h5pulsar",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     packages=find_packages(),
-    python_requires=">=3.7",
-    install_requires=[
-        "numpy>=1.16.3",
-        "astropy>=4.0,!=4.0.1,!=4.0.1.post1",
-        "enterprise-pulsar>=3.3.4",
-        "jplephem>=2.6",
-        "h5py>=3.9.0",
+    url="http://github.com/vhaasteren/h5pulsar/",
+    description="HDF5 read/write capabilities for Enterprise Pulsar objects",
+    license="MIT",
+    long_description=open("README.md").read(),
+    url="https://github.com/vhaasteren/h5pulsar",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
+    python_requires=">=3.9",
 )
